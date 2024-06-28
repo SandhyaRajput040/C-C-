6b059cd473172a573b72061475dffef0c4d959d9
Sandhya Rajput <168713659+SandhyaRajput040@users.noreply.github.com>
2024-06-28 16:56:41 +0530

    Swapping Using Third variable

---
+++ swapping.c
@@ -0,0 +1,19 @@
+#include<Stdio.h>
+void main(){
+
+int num1=10;
+int num2=20;
+int temp;
+printf("Number 1=%d\n",num1);
+printf("Number 2=%d\n",num2);
+  
+  //swapping 
+  temp=num1;
+  num1=num2;
+  num2=temp;
+  
+printf("After Swaaping\n");
+printf("Number 1=%d\n",num1);
+printf("Number 2=%d\n",num2);
+
+}
