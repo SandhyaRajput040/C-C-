a2d441ce35974ed5c1039808da431f1be66b9d5c
Sandhya Rajput <168713659+SandhyaRajput040@users.noreply.github.com>
2024-06-28 18:03:53 +0530

    Arithmetic  Operations in c

---
+++ Arithimatic.c
@@ -0,0 +1,18 @@
+#include<Stdio.h>
+void main(){
+
+int num1=20;
+ int num2=10;
+
+
+
+  
+printf("Arithimatic Operations:-\n");
+printf("Sum=%d\n",num1+num2);//30
+printf("Subtraction=%d\n",num1-num2);//10
+printf("Multiplicaion=%d\n",num1*num2);//200
+printf("Divide=%d\n",num1/num2);//2
+printf("Reminder=%d\n",num1%num2);//0
+
+
+}
