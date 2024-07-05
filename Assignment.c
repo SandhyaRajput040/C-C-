c0ef777c39676ca7b797ebe5daccee0af08b1589
Sandhya Rajput <168713659+SandhyaRajput040@users.noreply.github.com>
2024-07-05 20:20:21 +0530

    Create Assignment.c

---
+++ Assignment.c
@@ -0,0 +1,21 @@
+#include<Stdio.h>
+void main(){
+
+int num1=10;
+int num2=20;
+ 
+ num1=num2;//equal
+printf("%d\n",num1);
+ num1+=num2;//num1=num1+num2  plus equals
+printf("%d\n",num1);
+ num1-=num2;//num1=num1-num2  minus equals
+printf("%d\n",num1);
+ num1*=num2;//num1=num1*num2  multiple equals
+printf("%d\n",num1);
+ num1/=num2;//num1=num1/num2  divide equals
+printf("%d\n",num1);
+ num1%=num2;//num1=num1%num2   reminder equals 
+printf("%d\n",num1);
+ 
+ 
+}
