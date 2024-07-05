e4838fc50b9364779815978887938b322160e0c1
Sandhya Rajput <168713659+SandhyaRajput040@users.noreply.github.com>
2024-07-05 19:43:31 +0530

    Relational Operators in c

---
+++ Relational.c
@@ -0,0 +1,19 @@
+#include<Stdio.h>
+void main(){
+
+int num1=10;
+int num2=20;
+ /*meaning(Value) of true and false
+ true=1
+ false=0}*/
+
+
+printf("%d\n",num1>num2);//Greater Than
+printf("%d\n",num1<num2);//less than
+printf("%d\n",num1>=num2);//less than equal to
+printf("%d\n",num1<=num2);//greater than equal to
+printf("%d\n",num1==num2);//equal to
+printf("%d\n",num1!=num2);//not equal to
+
+
+}
