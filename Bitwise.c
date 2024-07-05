a5a1a0f203f2e57f9ea1f52b761651c0ed1b40cf
Sandhya Rajput <168713659+SandhyaRajput040@users.noreply.github.com>
2024-07-05 21:52:40 +0530

    Create Bitwise Operator in c

---
+++ Bitwise.c
@@ -0,0 +1,14 @@
+#include<Stdio.h>
+void main(){
+ 
+ 
+ 
+printf("%d\n",10&9);//Bitwise AND
+printf("%d\n",10|9);//Bitwise OR
+printf("%d\n",10^9);//Bitwise XOR
+printf("%d\n",10<<2);//Bitwise Left Shift
+printf("%d\n",10>>2);//Bitwise Right Shiht
+printf("%d\n",~10);//Bitwise Complement 
+
+
+}
